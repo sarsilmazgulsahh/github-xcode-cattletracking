0de4e823f101318313c9970746ac278b95d4f5bd
GÜLŞAH <gulsah.sarsilmaz@takipsan.com>
2016-05-24 18:36:00 +0300

    Initial Commit

---
+++ Test2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test2
+//
+//  Created by GÜLŞAH on 5/24/16.
+//  Copyright © 2016 GÜLŞAH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
